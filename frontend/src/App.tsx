ded827700741011db82f9de0d92f73480e1df4f2
vikalp-tomer <vikalptomar15@gmail.com>
2024-07-12 11:39:22 +0530

    added register form in frontend with react-hook-form and connected it with backend using react-query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Layout from "./layouts/Layout";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
@@ -14,6 +15,14 @@ const App = () => {
           }
         />
         <Route path="/search" element={<Layout>Search Page</Layout>} />
+        <Route
+          path="/register"
+          element={
+            <Layout>
+              <Register />
+            </Layout>
+          }
+        />
       </Routes>
     </Router>
   );
